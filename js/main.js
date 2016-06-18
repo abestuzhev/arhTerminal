f410240ea939c6ba5d99cedf6927ba77157ee928
Aleksey <aleksey.bestuzhev@gmail.com>
2016-06-18 17:09:08 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -14,16 +14,28 @@ $(document).ready(function(){
   });
 
   $('.buses_slider').slick({
-    autoplay: false,
-    autoplaySpeed: 5000,
-    dots: false,
-    arrows: true,
+
     infinite: true,
-    speed: 500,
-    adaptiveHeight: false,
+    speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 1,
-    variableWidth: true
+    slidesToScroll: 4,
+    // variableWidth: true
+    responsive: [
+    {
+      breakpoint: 1360,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 1060,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
   });
 
   $('.partners_slider').slick({
@@ -35,7 +47,43 @@ $(document).ready(function(){
     speed: 500,
     adaptiveHeight: false,
     slidesToShow: 6,
-    slidesToScroll: 1,
-    variableWidth: true
+    slidesToScroll: 6,
+    responsive: [
+    {
+      breakpoint: 1160,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5
+      }
+    },
+    {
+      breakpoint: 1060,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    },
+    {
+      breakpoint: 880,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 380,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
 });
